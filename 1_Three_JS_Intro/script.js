2925b1c20b15334adedfca129e2feb8981f5e09e
Harsh-Arya <harsharyaa666@gmail.com>
2024-12-05 16:25:44 +0530

    xyz

--- 1_Three_JS_Intro/script.js
+++ 1_Three_JS_Intro/script.js
@@ -10,7 +10,8 @@ const scene = new THREE.Scene();
 //Making Object with (geometry+material) & adding it into to make an object via mesh and then add it into scene
 
 const geometry = new THREE.BoxGeometry(2,2,2);
-const material = new THREE.MeshBasicMaterial({color: "lightsalmon"});
+const material = new THREE.MeshBasicMaterial({color: "blue"});
+
 const boxObj = new THREE.Mesh(geometry,material);
 scene.add(boxObj);
 
@@ -21,15 +22,18 @@ scene.add(boxObj);
 const size = {
     width: 700,
     height: 500,
-}
-const camera = new THREE.PerspectiveCamera( 45, size.width / size.height, 1, 1000 );
-scene.add( camera );
+};
+
+// const camera = new THREE.PerspectiveCamera( 45, size.width / size.height, 1, 1000 );
+const camera = new THREE.PerspectiveCamera(75, size.width / size.height);
 camera.position.z = 4;
+
+scene.add( camera );
 
 
 //Now we are defining renderer & give scene camera to it also give the details about size to renderer(director)
 
-const renderer = new THREE.WebGLRenderer();
+const renderer = new THREE.WebGLRenderer({ canvas: target });
 renderer.setSize(size.width, size.height)
 
 renderer.render(scene, camera);
