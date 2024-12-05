d43453007714df6b6718327841844e8814ed963e
Harsh-Arya <harsharyaa666@gmail.com>
2024-12-05 15:43:14 +0530

    Now we are defining camera & adding it into scene

--- 1_Three_JS_Intro/script.js
+++ 1_Three_JS_Intro/script.js
@@ -1,7 +1,24 @@
 
 // console.log("Hello I am Harsh Arya");
 // console.log(THREE);
-
+const target = document.querySelector(".sceen");
 const scene = new THREE.Scene();
 
 
+//Making Object with (geometry+material) & adding it into to make an object via mesh and then add it into scene
+
+const geometry = new THREE.BoxGeometry(2,2,2);
+const material = new THREE.MeshBasicMaterial({color: "lightsalmon"});
+const boxObj = new THREE.Mesh(geometry,material);
+scene.add(boxObj);
+
+
+
+//Now we are defining camera & adding it into scene
+
+const size = {
+    width: 700,
+    height: 500,
+}
+const camera = new THREE.PerspectiveCamera( 45, width / height, 1, 1000 );
+scene.add( camera );
